3beab5f037a49e2bc02201169d9045ea335cca26
start <start@DESKTOP-L1PH92H>
2024-09-12 15:40:52 +0300

    03 Layers Arch. Pattern - Data Access Layer

---
+++ LinkDev.IKEA.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace LinkDev.IKEA.DAL.Data
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        //second
+        public ApplicationDbContext() : base()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+         => optionsBuilder.UseSqlServer("Server = .; Database = IKEA; Trusted_COnnection = True; TrustServerCertificate = True");
+
+      
+    }
+
+}
